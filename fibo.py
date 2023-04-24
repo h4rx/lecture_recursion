20a138b5bb89172faf1f8222392adfd9d4ed6989
Marek Madar <marek.madar@finshape.com>
2023-04-24 09:47:33 +0200

    add sequence list to show fibonacci elements

--- fibo.py
+++ fibo.py
@@ -6,8 +6,16 @@ def recursive_nth_fibo(n):
 
 
 def main():
-    fibo = recursive_nth_fibo(5)
-    print(fibo)
+    n = int(input("Number of Fibonacci elements: "))
+    fibo = recursive_nth_fibo(n)
+    seq = []
+    """
+    for i in range(n):
+        n_fibo = recursive_nth_fibo(i)
+        seq.append(n_fibo)
+    """
+    seq = [recursive_nth_fibo(i) for i in range(n)]
+    print(seq)
 
 if __name__ == "__main__":
     main()
