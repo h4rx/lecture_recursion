2f7288a0bd5874a42952aea21ee32cfb0ff4145c
Marek Madar <marek.madar@finshape.com>
2023-04-24 09:25:33 +0200

    add function for fibonacci

--- fibo.py
+++ fibo.py
@@ -1,7 +1,13 @@
+def recursive_nth_fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return (recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2))
+
 
 def main():
-    pass
-
+    fibo = recursive_nth_fibo(5)
+    print(fibo)
 
 if __name__ == "__main__":
     main()
